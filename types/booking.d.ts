1c36b40e8704bc0a5cc5abd95befb696bfd6544f
tjarbo <16938041+tjarbo@users.noreply.github.com>
2021-11-14 15:22:32 +0000

    🏷 Added types

---
+++ types/booking.d.ts
@@ -0,0 +1,47 @@
+/**
+ * Interface of a passenger
+ */
+export interface Passenger {
+  /** first name of passenger */
+  firstName: string;
+
+  /** last name of passenger */
+  lastName: string;
+
+  /** birthrate of passenger */
+  birthrate: string;
+
+  /** gender of passenger */
+  gender: string;
+}
+
+/**
+ * Interface of a credit card
+ */
+export interface CreditCard {
+  /** holder of credit card */
+  holder: string;
+
+  /** number of credit card */
+  number: string;
+
+  /** expiration date of credit card */
+  expire: string;
+}
+
+/**
+ * Interface of a booking
+ */
+export interface Booking {
+  /** unique number that identifies the booking */
+  number: string;
+
+  /** list of all passengers */
+  passengers: Passenger[];
+
+  /** credit card used for booking */
+  creditCard: CreditCard;
+
+  /** flights for booking */
+  flights: any[];
+}
